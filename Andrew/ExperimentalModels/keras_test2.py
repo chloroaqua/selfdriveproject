99bd1e01565e56409d21688a9aca47222c34e98f
chloroaqua <asimpson@wiley.com>
2017-05-12 21:54:46 -0600

    CS231n milestone paper start

--- Andrew/ExperimentalModels/keras_test2.py
+++ Andrew/ExperimentalModels/keras_test2.py
@@ -8,7 +8,7 @@ from keras.losses import mean_squared_error
 import keras_help
 
 from keras.models import Sequential
-from keras.layers import Dense, Activation, Conv2D, MaxPooling2D, Flatten
+from keras.layers import Dense, Activation, Conv2D, MaxPooling2D, Flatten, BatchNormalization
 
 training_dataset_path = "M:\\selfdrive\\SelfDrivingData\\test_out2\\training"
 training_labels = np.load(os.path.join(training_dataset_path, 'training_center_labels.npy'))
@@ -33,9 +33,8 @@ for val in keras_help.generate_arrays_from_file_v2(training_labels, training_ind
 
 print(training_labels.shape)
 model = Sequential()
-layer = Conv2D(32, (3, 3), activation='relu', input_shape=(240, 320, 3))
-print(layer.get_config())
-model.add(layer)
+model.add(Conv2D(32, (3, 3), activation='relu', input_shape=(240, 320, 3)))
+model.add(BatchNormalization())
 model.add(Conv2D(32, (3, 3), activation='relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 model.add(Conv2D(64, (3, 3), activation='relu'))
@@ -51,7 +50,7 @@ model.fit_generator(keras_help.generate_arrays_from_file_v2(training_labels, tra
 model.fit_generator(keras_help.generate_arrays_from_file_v2(training_labels, training_index_center, image_base_path_training, 32),
                     steps_per_epoch=training_labels.shape[0] // 32,
                     validation_data=keras_help.generate_arrays_from_file_v2(validation_labels, validation_index_center, image_base_path_validation, 32),
-                    validation_steps=validation_labels.shape[0] // 32, epochs=10, verbose=1)
+                    validation_steps=validation_labels.shape[0] // 32, epochs=40, verbose=1)
 
 
 
